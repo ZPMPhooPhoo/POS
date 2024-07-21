14bf064a6b056f7edd3cde2af72e22b15da09c42
zinphoomon <zinphoomon.zpm@gmail.com>
2024-07-21 23:45:58 +0630

    configuration mongodb

--- src/main/java/com/lumina/pos/controller/LoginController.java
+++ src/main/java/com/lumina/pos/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.lumina.pos.controller;
 import java.util.HashMap;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.lumina.pos.common.Constant;
 import com.lumina.pos.model.User;
+import com.lumina.pos.service.UserService;
 
 import ch.qos.logback.classic.Logger;
 
@@ -17,6 +19,8 @@ import ch.qos.logback.classic.Logger;
 public class LoginController {
      private static final Logger logger = (Logger) LoggerFactory.getLogger(LoginController.class);
 
+     @Autowired
+     private UserService userService;
      //create user 
       @RequestMapping(value = "/register", method=RequestMethod.GET)
     public String Register(User user,Model model) {
@@ -35,6 +39,7 @@ public class LoginController {
     public String register(User user,Model model) {
         try {
             logger.info("Register Screen!"+user);
+            userService.CreateUser(user);
         } catch (Exception e) {
             logger.warn(e.getLocalizedMessage());
             return "redirect:/common/error";
---
+++ src/main/java/com/lumina/pos/service/UserService.java
@@ -0,0 +1,17 @@
+package com.lumina.pos.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lumina.pos.model.User;
+import com.lumina.pos.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepo;
+
+    public void CreateUser(User user){
+        userRepo.save(user);
+    }
+}
